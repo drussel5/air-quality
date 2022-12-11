2e974982ec77ce13d3ab88caf5d1f36d7fa6f3f9
drussel5 <russell.devon91@gmail.com>
2022-12-11 13:50:05 -0500

    Cleaning up comments and debugging.

--- src/Hooks/useMeasurements.js
+++ src/Hooks/useMeasurements.js
@@ -1,16 +1,14 @@
 import { useEffect } from 'react';
 
 export const useMeasurements = (props) => {
-    // empty and need to have values
     const {city, setMeasurements} = props;
-    // debugger;
+    
     useEffect(() => {
         if (city) {
             const url = `https://api.openaq.org/v2/latest?city=${city}`
             fetch(url)
                 .then((response) => response.json())
                 .then((data) => {
-                    // debugger;
                     setMeasurements(data.results);
                 })
                 .catch((err) => {
